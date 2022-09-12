14ee16f39da243e668dd80bc7ded8fb324672f1f
Maximilien Mellen <17215508+maxmellen@users.noreply.github.com>
2022-09-12 12:05:18 +0200

    hani

---
+++ src/lib/hani.ts
@@ -0,0 +1,8 @@
+import txt from './wiki.txt?raw'
+
+export type Hani = Record<'trad' | 'simp' | 'shin', string>
+export const hani: Hani[] = txt
+  .split('\n')
+  .filter((line) => !line.startsWith('#') && line.length >= 3)
+  .map((line) => line.slice(0, 3).split(''))
+  .map(([trad, simp, shin]) => ({ trad, simp, shin } as Hani))
